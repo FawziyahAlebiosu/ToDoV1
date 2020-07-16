5bf9094402f8f97fbfa2b9154856733de9ce3bc8
Fawziyah <fawziyah.alebiosu@gmail.com>
2020-07-16 14:17:22 -0400

    now has add, and save functionality

--- app/src/main/java/com/example/android/disherv1/itemAdapter.java
+++ app/src/main/java/com/example/android/disherv1/itemAdapter.java
@@ -1,29 +1,35 @@
 package com.example.android.disherv1;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class itemAdapter extends RecyclerView.Adapter<itemAdapter.viewHolder>{
-    List<String> toDoItems;
-    public itemAdapter(List<String>toDoItems){
-        this.toDoItems = toDoItems;
+
+    public interface onLongClickListener{
+        void onItemLongClicked(int position);
 
     }
+    List<String> toDoItems = new ArrayList<>();
+    onLongClickListener longClickListener;
+    public itemAdapter(List<String>toDoItems, onLongClickListener longClickListener){
+        this.toDoItems = toDoItems;
+    }
+
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //use a layout view to inflate a view; inflate means to instantiate the view
-        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.Layout.simple_list_item_1, parent, false);
-        return new RecyclerView.ViewHolder(todoView);
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent,false );
+        return new viewHolder(todoView);
 
     }
 
@@ -43,11 +49,18 @@ public class itemAdapter extends RecyclerView.Adapter<itemAdapter.viewHolder>{
 
             public viewHolder(@NonNull View itemView) {
                 super(itemView);
-                currentItem = itemView.findViewById()
+                currentItem = itemView.findViewById(android.R.id.text1);
             }
-
+//bind will update view inside the viewholder with the current string
         public void bind(String item) {
                 currentItem.setText(item);
+                currentItem.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        longClickListener.onItemLongClicked(getAdapterPosition());
+                        return true;
+                    }
+                });
         }
     }
 
